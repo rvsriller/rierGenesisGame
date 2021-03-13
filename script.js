05367c5b30bd5505087b940bc8c9a2cabfb7d663
Riller <rvsriller@gmail.com>
2021-03-13 05:47:28 -0300

    Still building

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ let score = 0;
 3 - blau (azul)
 */
 
+//Find grids in html code
 const blau = document.querySelector('.blau');
 const rot = document.querySelector('.rot');
 const grun = document.querySelector('.grun');
 const gelb = document.querySelector('.gelb');
 
-//find a order of colors to present in game
+
+
+//create an order of colors to present in game
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder; //size of vector equal to number generated
-    clickOrder = [];
+    clickedOrder = [];
     
     //Light On following the numbers generated - Order of color
 
@@ -29,12 +32,13 @@ let shuffleOrder = () => {
 }
 
 
-//light color generate
+
+//light color ON - generate
 let lightColor = (element, number) => {
-    time = time * 500;
+    number = number * 500;
     setTimeout(() => {
         element.classList.add('selected');
-    }, tempo -250);
+    }, number -250);
 
     //turn off
     setTimeout(() => {
@@ -42,3 +46,90 @@ let lightColor = (element, number) => {
     })
 
 }
+
+
+//Comparing order of game colors and player color selections
+let checkOrder = () => {
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            gameOver();
+            break;
+        }
+    }
+
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\n Você acertou! Iniciando próximo nível`);
+        nextLevel();
+    }
+}
+
+
+//Verifying if all clicks of player are equal to game colors
+let click = (color) => {
+    clickOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+}
+
+
+//Create color element function
+let createColorElement = (color) => {
+    if(color == 0){
+        return grun;
+    } else if(color == 1){
+        return rot;
+    } else if(color == 2){
+        return gelb;
+    } else if(color == 3) {
+        return blau;
+    }
+}
+
+
+//Next level function
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+
+
+//Game over function
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\n Você perdeu o jogo \n Clique em OK para iniciar novo jogo`);
+    order = [];
+    clickedOrder = [];
+    //after reset all parameters, it can starts a new game
+
+    playGame();
+}
+
+//reset level and start game
+let playGame = () => {
+    alert('Bem vindo ao Gênesis. \n Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+
+/* Click listening
+grun.addEventListener('click', click(0));
+rot.addEventListener('click', click(1));
+gelb.addEventListener('click', click(2));
+blau.addEventListener('click', click(3)); */
+
+
+//Click events for colors 
+grun.onclick = () => click(0);
+rot.onclick = () => click(1);
+gelb.onclick = () => click(2);
+blau.onclick = () => click(3);
+
+
+//Start game after alert
+playGame();
